7ccd6d85241b524f55926f9102947d2e9de0452f
Peter <diosdadopeter@gmail.com>
2023-09-12 22:46:00 -0700

    fix: routes to auth if user access top w/o login

--- src/pages/TopTen.tsx
+++ src/pages/TopTen.tsx
@@ -1,28 +1,32 @@
 import { useEffect, useState } from "react"
 import { getAccessToken } from '../services/getAccessToken';
+import { redirectToAuthCodeFlow } from "../services/redirectToAuthCodeFlow";
 
 export default function TopTen() {
-    const [tracks, setTracks] = useState<object[] | undefined>([])
-    const[loading, setLoading] = useState<boolean>(true)
+    const [tracks, setTracks] = useState<object[]>([])
+    const [loading, setLoading] = useState<boolean>(true)
 
     useEffect(() => {
-
         async function fetchTracks() {
+            setLoading(true)
             const clientId = import.meta.env.VITE_CLIENT_ID
             const params = new URLSearchParams(window.location.search)
             const code = params.get('code')
-            console.log(code)
-            const token = await getAccessToken(clientId, code!);
 
+            if (!code) {
+                await redirectToAuthCodeFlow(clientId)
+                return
+            }
+            const token = await getAccessToken(clientId, code)
+            console.log('token set' + token)
             const response = await fetch("https://api.spotify.com/v1/me/top/tracks?time_range=short_term&offset=0", {
                 method: "GET", headers: { Authorization: `Bearer ${token}` }
             })
-
-            setLoading(false)
-            const songs : {items: object[]}= await response.json()
+            const songs : {items: object[]} = await response.json()
             setTracks(songs.items)
-            console.log(tracks)
+            setLoading(false)
         }
+
         fetchTracks()
     },[])
 
@@ -31,10 +35,10 @@ export default function TopTen() {
         {loading ? <h1>Loading tracks...</h1> : <p>Top Ten!</p>}
         {
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            !loading && tracks ? tracks.map((song : any) => {
+            !loading && tracks?.length > 0 ? tracks?.map((song : any) => {
                 return <p>{song.name!} - {song.artists[0].name!}</p>
             })
-        :null}
+        : null}
     </div>
   )
 }
