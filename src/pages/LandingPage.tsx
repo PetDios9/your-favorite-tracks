7228cda64a43f189792bc7101b65814068f5a9fa
Peter <diosdadopeter@gmail.com>
2023-09-20 17:49:26 -0700

    style: styling changes and renamed landing

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,9 +3,10 @@ import authenticate from "../services/authenticate";
 export default function LandingPage() {
   return (
     <div className="login-container">
-    <h2>Spotify Month In Review!</h2>
-    <h3>Click here to login and see your last month's top tracks and artists!</h3>
-    <button onClick={authenticate}>Log In Using Spotify</button>
+      <h2 className="top-tracks-heading">Your Favorites In Review!</h2>
+      <h3>Click here to login and see your favorite tracks on Spotify!</h3>
+      <button onClick={authenticate}>Log In Using Spotify</button>
+      <h6 className="top-tracks-heading">Created by: Peter Diosdado</h6>
   </div>
   )
 }
