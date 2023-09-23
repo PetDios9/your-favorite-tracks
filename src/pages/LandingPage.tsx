5bff936c8c485272dac1152079a8eae066fefc23
Peter <diosdadopeter@gmail.com>
2023-09-23 00:10:44 -0700

    style: changed desc text on homepage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -6,7 +6,7 @@ export default function LandingPage() {
   return (
     <div className="login-container">
       <h2 className="top-tracks-heading">Your Favorites In Review!</h2>
-      <h3>Click here to login and see your favorite tracks on Spotify!</h3>
+      <h3>Click here to login and see your favorite tracks on Spotify and turn them into a playlist!</h3>
       <button onClick={authenticate}>Log In Using Spotify</button>
       <h6 className="top-tracks-heading">Created by: Peter Diosdado</h6>
       <img className="demo-gif-desktop" src={desktopDemo} alt="demo gif of webapp showing track cards and layout" />
