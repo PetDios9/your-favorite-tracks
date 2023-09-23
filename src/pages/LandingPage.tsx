8d95ba3850058f3127829e97808cbebdcd49cc75
Peter <diosdadopeter@gmail.com>
2023-09-22 21:49:29 -0700

    style: added disclaimer and max width to home

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -7,6 +7,10 @@ export default function LandingPage() {
       <h3>Click here to login and see your favorite tracks on Spotify!</h3>
       <button onClick={authenticate}>Log In Using Spotify</button>
       <h6 className="top-tracks-heading">Created by: Peter Diosdado</h6>
+      <p>*Note: This web app is still not publically usable due to Spotify's API restrictions and 
+        is currently under review for public release. Please contact me at peterdiosdadochavez@gmail.com 
+        if you would like a demo or to be added as an authorized user.
+      </p>
   </div>
   )
 }
